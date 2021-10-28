fe2752ff8284bc9f5b0270f96eec9e6baf48324d
github Ko-Eunseo <github koyk0408@naver.com>
2021-10-28 14:07:54 +0900

    Hbaf gnb

--- js/common.js
+++ js/common.js
@@ -72,7 +72,16 @@ $('.toggle').click(function(){
     $('#gnb > ul').addClass('dropdown'),
     $('#gnb > ul > li').addClass('dropdowncontent'),
     $('#gnb > ul > i.exit').css('display','block'),
+    $('main').css('display','none'),
     $('#gnb').css('display','inline-block'),
     $('header > nav > ul').css('left','30%');
   }
 });
+
+// $('#gnb > ul.dropdown > li').click(function(){
+//   if($('.dropdowncontainer').is(':visible')){
+//     $('#gnb').css('display','none'),
+//     $('#logo').css('margin-left','5%'),
+//     $('main').css('display','block');
+//   }
+// });
